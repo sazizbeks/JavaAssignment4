f72e7e14b023ce5d8cc232eb0a26739469e56c3f
sazizbeks <sazizbeks@gmail.com>
2020-10-26 21:08:09 +0600

    DB, Book model

---
+++ src/main/java/kz/edu/astanait/DB.java
@@ -0,0 +1,25 @@
+package kz.edu.astanait;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+
+    private static Connection connection;
+
+    private DB() {
+    }
+
+    public static Connection getConnection() {
+        if(connection==null){
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/assignment_week_8?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+                        "root","");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
--- src/main/java/kz/edu/astanait/models/Book.java
+++ src/main/java/kz/edu/astanait/models/Book.java
@@ -4,7 +4,7 @@ public class Book {
     private String ISBN;
     private String name;
     private String author;
-    private int count_of_copy;
+    private Integer count_of_copy;
 
     public Book(String ISBN, String name, String author, int count_of_copy) {
         this.ISBN = ISBN;
@@ -37,11 +37,11 @@ public class Book {
         this.author = author;
     }
 
-    public int getCount_of_copy() {
+    public Integer getCount_of_copy() {
         return count_of_copy;
     }
 
-    public void setCount_of_copy(int count_of_copy) {
+    public void setCount_of_copy(Integer count_of_copy) {
         this.count_of_copy = count_of_copy;
     }
 }
