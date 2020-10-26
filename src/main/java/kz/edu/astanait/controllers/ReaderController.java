fea37f849db90f8eed6e5fc1ef8c2520ee359698
sazizbeks <sazizbeks@gmail.com>
2020-10-26 22:20:18 +0600

    Implemented ReaderController.update

--- src/main/java/kz/edu/astanait/controllers/ReaderController.java
+++ src/main/java/kz/edu/astanait/controllers/ReaderController.java
@@ -1,8 +1,11 @@
 package kz.edu.astanait.controllers;
 
+import kz.edu.astanait.DB;
 import kz.edu.astanait.controllers.interfaces.IReaderController;
 import kz.edu.astanait.models.Reader;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ReaderController implements IReaderController {
@@ -13,7 +16,26 @@ public class ReaderController implements IReaderController {
 
     @Override
     public void update(Reader entity) {
+        StringBuilder sb = new StringBuilder("UPDATE READERS SET ");
 
+        if (entity.getFirstName() != null) sb.append("FNAME=?,");
+        if (entity.getLastName() != null) sb.append("LNAME=?,");
+
+        sb.deleteCharAt(sb.length() - 1); // Deletes last coma (,)
+        sb.append(" WHERE ID=?");
+
+        try {
+            PreparedStatement preparedStatement = DB.getConnection().prepareStatement(sb.toString());
+
+            int i = 1;
+            if (entity.getFirstName() != null) preparedStatement.setString(i++, entity.getFirstName());
+            if (entity.getLastName() != null) preparedStatement.setString(i++, entity.getLastName());
+            preparedStatement.setInt(i, entity.getId());
+
+            preparedStatement.execute();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @Override
