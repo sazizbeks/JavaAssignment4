b2bfb464823762a69ef05a75514168ba47120bd7
sazizbeks <sazizbeks@gmail.com>
2020-10-29 21:20:33 +0600

    Add code blocks for work with REST to web.xml

--- src/main/java/kz/edu/astanait/Test.java
+++ src/main/java/kz/edu/astanait/Test.java
@@ -1,7 +1,32 @@
 package kz.edu.astanait;
 
+//import com.sun.jersey.api.client.Client;
+//import com.sun.jersey.api.client.ClientResponse;
+//import com.sun.jersey.api.client.WebResource;
+//import com.sun.jersey.api.client.config.ClientConfig;
+//import com.sun.jersey.api.client.config.DefaultClientConfig;
+import kz.edu.astanait.controllers.ReaderController;
+
+import javax.ws.rs.core.MediaType;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Some commit");
+//        ReaderController rd = new ReaderController();
+//        rd.getAll().forEach(System.out::println);
+
+//        String REST_URI = "http://localhost:8080/Assignment4_war/api";
+//
+//        ClientConfig config = new DefaultClientConfig();
+//        Client client = Client.create(config);
+//        WebResource service = client.resource(REST_URI);
+//
+//        WebResource readerService = service.path("/reader");
+//
+//        System.out.println();
+//
+//        String s = readerService.path("/getAll").accept(MediaType.APPLICATION_JSON_TYPE).get(ClientResponse.class).toString();
+//        System.out.println(s);
+//
+//        readerService.getURI();
     }
 }
